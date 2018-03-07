a89d808b1d06002b264bb9821b17cf175d9fe508
Furkan Üzümcü <furkanuzumcu@gmail.com>
2018-03-07 13:06:49 -0500

    - Add field names. - Add card brand enum and converter methods. - Add funding type enum and converter methods.

--- include/QStripe/Card.h
+++ include/QStripe/Card.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <QObject>
+#include <QVector>
 
 namespace QStripe
 {
@@ -9,11 +10,98 @@ class Card : public QObject
     Q_OBJECT
 
 public:
+    static const QString FIELD_OBJECT;
+    static const QString FIELD_ADDRESS_CITY;
+    static const QString FIELD_ADDRESS_COUNTRY;
+
+    static const QString FIELD_ADDRESS_LINE1;
+    static const QString FIELD_ADDRESS_LINE1_CHECK;
+    static const QString FIELD_ADDRESS_LINE2;
+
+    static const QString FIELD_ADDRESS_STATE;
+    static const QString FIELD_ADDRESS_ZIP;
+    static const QString FIELD_ADDRESS_ZIP_CHECK;
+
+    static const QString FIELD_BRAND;
+    static const QString FIELD_COUNTRY;
+    static const QString FIELD_CURRENCY;
+
+    static const QString FIELD_CUSTOMER;
+    static const QString FIELD_CVC_CHECK;
+    static const QString FIELD_EXP_MONTH;
+
+    static const QString FIELD_EXP_YEAR;
+    static const QString FIELD_FINGERPRINT;
+    static const QString FIELD_FUNDING;
+
+    static const QString FIELD_NAME;
+    static const QString FIELD_LAST4;
+    static const QString FIELD_ID;
+
+    static const QString FIELD_TOKENIZATION_METHOD;
+
+    enum CardBrand {
+        AmericanExpress,
+        Discover,
+        JCB,
+        DinersClub,
+        Visa,
+        MasterCard,
+        Unknown
+    };
+
+    enum FundingType {
+        FundingCredit,
+        FundingDebit,
+        FundingPrePaid,
+        FundingUnknown
+    };
+
+public:
     explicit Card(QObject *parent = nullptr);
 
-signals:
+private:
+    /**
+     * @brief Returns the card number prefixes for the brand.
+     * Card number prefixes are based on http://en.wikipedia.org/wiki/Bank_card_number#Issuer_identification_number_.28IIN.29
+     * @return QVector<QString>
+     */
+    QVector<QString> prefixes(CardBrand brand) const;
 
-public slots:
+    /**
+     * @brief Returns the max card number length based on the brand.
+     * @param brand
+     * @return int
+     */
+    int maxCardNumberLenght(CardBrand brand) const;
+
+    /**
+     * @brief Returns the name of the brand.
+     * @param brand
+     * @return QString
+     */
+    QString cardBrandName(CardBrand brand) const;
+
+    /**
+     * @brief Returns the CardBrand enum from the name.
+     * @param name
+     * @return CardBrand
+     */
+    CardBrand cardBrandType(const QString &name);
+
+    /**
+     * @brief Returns the string representation of the funding type.
+     * @param type
+     * @return QString
+     */
+    QString fundingTypeString(FundingType type) const;
+
+    /**
+     * @brief Returns the FundingType enum for the given name.
+     * @param name
+     * @return FundingType
+     */
+    FundingType fundingType(const QString &name) const;
 };
 
 }
--- src/Card.cpp
+++ src/Card.cpp
@@ -3,10 +3,187 @@
 namespace QStripe
 {
 
+const QString Card::FIELD_OBJECT = "object";
+const QString Card::FIELD_ADDRESS_CITY = "address_city";
+const QString Card::FIELD_ADDRESS_COUNTRY = "address_country";
+
+const QString Card::FIELD_ADDRESS_LINE1 = "address_line1";
+const QString Card::FIELD_ADDRESS_LINE1_CHECK = "address_line1_check";
+const QString Card::FIELD_ADDRESS_LINE2 = "address_line2";
+
+const QString Card::FIELD_ADDRESS_STATE = "address_state";
+const QString Card::FIELD_ADDRESS_ZIP = "address_zip";
+const QString Card::FIELD_ADDRESS_ZIP_CHECK = "address_zip_check";
+
+const QString Card::FIELD_BRAND = "brand";
+const QString Card::FIELD_COUNTRY = "country";
+const QString Card::FIELD_CURRENCY = "currency";
+
+const QString Card::FIELD_CUSTOMER = "customer";
+const QString Card::FIELD_CVC_CHECK = "cvc_check";
+const QString Card::FIELD_EXP_MONTH = "exp_month";
+
+const QString Card::FIELD_EXP_YEAR = "exp_year";
+const QString Card::FIELD_FINGERPRINT = "fingerprint";
+const QString Card::FIELD_FUNDING = "funding";
+
+const QString Card::FIELD_NAME = "name";
+const QString Card::FIELD_LAST4 = "last4";
+const QString Card::FIELD_ID = "id";
+
+const QString Card::FIELD_TOKENIZATION_METHOD = "tokenization_method";
+
 Card::Card(QObject *parent)
     : QObject(parent)
 {
 
+}
+
+QVector<QString> Card::prefixes(CardBrand brand) const
+{
+    QVector<QString> numberPrefixes;
+
+    if (brand == CardBrand::AmericanExpress) {
+        numberPrefixes = QVector<QString> {"34", "37"};
+    }
+    else if (brand == CardBrand::Discover) {
+        numberPrefixes = QVector<QString> {"60", "62", "64", "65"};
+    }
+    else if (brand == CardBrand::JCB) {
+        numberPrefixes = QVector<QString> {"35"};
+    }
+    else if (brand == CardBrand::DinersClub) {
+        numberPrefixes = QVector<QString> {
+            "300", "301", "302", "303", "304",
+            "305", "309", "36", "38", "39"
+        };
+    }
+    else if (brand == CardBrand::Visa) {
+        numberPrefixes = QVector<QString> {"4"};
+    }
+    else if (brand == CardBrand::MasterCard) {
+        numberPrefixes = QVector<QString> {
+            "2221", "2222", "2223", "2224",
+            "2225", "2226", "2227", "2228",
+            "2229", "223", "224", "225",
+            "226", "227", "228", "229",
+            "23", "24", "25", "26",
+            "270", "271", "2720", "50",
+            "51", "52", "53", "54",
+            "55", "67"
+        };
+    }
+
+    return numberPrefixes;
+}
+
+int Card::maxCardNumberLenght(CardBrand brand) const
+{
+    // This is the standard one.
+    int length = 16;
+
+    if (brand == CardBrand::AmericanExpress) {
+        length = 15;
+    }
+    else if (brand == CardBrand::DinersClub) {
+        length = 14;
+    }
+
+    return length;
+}
+
+QString Card::cardBrandName(CardBrand brand) const
+{
+    QString name;
+    if (brand == CardBrand::AmericanExpress) {
+        name = "American Express";
+    }
+    else if (brand == CardBrand::Discover) {
+        name = "Discover";
+    }
+    else if (brand == CardBrand::JCB) {
+        name = "JCB";
+    }
+    else if (brand == CardBrand::DinersClub) {
+        name = "Diners Club";
+    }
+    else if (brand == CardBrand::Visa) {
+        name = "Visa";
+    }
+    else if (brand == CardBrand::MasterCard) {
+        name = "MasterCard";
+    }
+    else if (brand == CardBrand::Unknown) {
+        name = "Unknown";
+    }
+
+    return name;
+}
+
+Card::CardBrand Card::cardBrandType(const QString &name)
+{
+    CardBrand brand;
+    if (name == "American Express") {
+        brand = CardBrand::AmericanExpress;
+    }
+    else if (name == "Discover") {
+        brand = CardBrand::Discover;
+    }
+    else if (name == "JCB") {
+        brand = CardBrand::JCB;
+    }
+    else if (name == "Diners Club") {
+        brand = CardBrand::DinersClub;
+    }
+    else if (name == "Visa") {
+        brand = CardBrand::Visa;
+    }
+    else if (name == "MasterCard") {
+        brand = CardBrand::MasterCard;
+    }
+    else {
+        brand = CardBrand::Unknown;
+    }
+
+    return brand;
+}
+
+QString Card::fundingTypeString(FundingType type) const
+{
+    QString name;
+    if (type == FundingType::FundingCredit) {
+        name = "credit";
+    }
+    else if (type == FundingType::FundingDebit) {
+        name = "debit";
+    }
+    else if (type == FundingType::FundingPrePaid) {
+        name = "prepaid";
+    }
+    else if (type == FundingType::FundingUnknown) {
+        name = "unknown";
+    }
+
+    return name;
+}
+
+Card::FundingType Card::fundingType(const QString &name) const
+{
+    FundingType type;
+    if (name == "credit") {
+        type = FundingType::FundingCredit;
+    }
+    else if (name == "debit") {
+        type = FundingType::FundingDebit;
+    }
+    else if (name == "prepaid") {
+        type = FundingType::FundingPrePaid;
+    }
+    else {
+        type = FundingType::FundingUnknown;
+    }
+
+    return type;
 }
 
 }
