ed74964e8d7b2f609c66591024e4600692734e08
Furkan Üzümcü <furkanuzumcu@gmail.com>
2018-03-06 20:13:42 -0500

    Add the necessary properties and signals.

--- include/QStripe/Stripe.h
+++ include/QStripe/Stripe.h
@@ -1,5 +1,10 @@
 #pragma once
 #include <QObject>
+#include <QQmlListProperty>
+#include <QVector>
+// Stripe
+#include "Customer.h"
+#include "NetworkUtils.h"
 
 namespace QStripe
 {
@@ -8,12 +13,143 @@ class Stripe : public QObject
 {
     Q_OBJECT
 
+    Q_PROPERTY(QString publishableKey READ publishableKey WRITE setPublishableKey NOTIFY publishableKeyChanged)
+    Q_PROPERTY(QString secretKey READ secretKey WRITE setSecretKey NOTIFY secretKeyChanged)
+    Q_PROPERTY(QString apiVersion READ apiVersion WRITE setApiVersion NOTIFY apiVersionChanged)
+
+    Q_PROPERTY(QQmlListProperty<Customer> customers READ customers)
+
 public:
     explicit Stripe(QObject *parent = nullptr);
 
-signals:
+    /**
+     * @brief Returns the publishable key.
+     * @return QString
+     */
+    QString publishableKey() const;
 
-public slots:
+    /**
+     * @brief Sets the current publishable key. This change does not effect the currently running requests. But any request after this change will use this key.
+     * @param key
+     */
+    void setPublishableKey(const QString &key);
+
+    /**
+     * @brief Returns the secret key.
+     * @return
+     */
+    QString secretKey() const;
+
+    /**
+     * @brief Sets the current secret key. This change does not effect the currently running requests. But any request after this change will use this key.
+     * @param key
+     */
+    void setSecretKey(const QString &key);
+
+    /**
+     * @brief The current API version. The default value is empty and uses the latest API version.
+     * @return QString
+     */
+    QString apiVersion() const;
+
+    /**
+     * @brief Sets the current api version to the given one. This change does not effect the currently running requests. But any request after this change will
+     * use this key.
+     * @param version
+     */
+    void setApiVersion(const QString &version);
+
+    /**
+     * @brief Returns the list of customer currently attached to this instance.
+     * @return QQmlListProperty<Customer>
+     */
+    QQmlListProperty<Customer> customers();
+
+    /**
+     * @brief Appends a new customer to the list.
+     * @param customer
+     */
+    void appendCustomer(Customer *customer);
+
+    /**
+     * @brief Returns the number of customers that belong to this instance.
+     * @return int
+     */
+    int customerCount() const;
+
+    /**
+     * @brief Returns the customer.
+     * @param index
+     * @return Customer
+     */
+    Customer *customer(int index) const;
+
+    /**
+     * @brief Clears all of the customer instances that belong to this instance.
+     */
+    void clearCustomers();
+
+signals:
+    /**
+     * @brief Emitted when the `publishableKey` property changes.
+     */
+    void publishableKeyChanged();
+
+    /**
+     * @brief Emitted when the `secretKey` property changes.
+     */
+    void secretKeyChanged();
+
+    /**
+     * @brief Emitted when the `apiVersion` property changes.
+     */
+    void apiVersionChanged();
+
+private:
+    QString m_PublishableKey,
+            m_SecretKey,
+            m_APIVersion;
+    QVector<Customer *> m_Customers;
+    NetworkUtils m_NetworkUtils;
+
+private:
+    /**
+     * @brief This is connected to the apiVersionChanged() signal. When the API version changes, the header will also change.
+     */
+    void updateVersionHeader();
+
+    /**
+     * @brief This is connected to the publishableKeyChanged() signal. When the publishable key changes, the header will also change.
+     */
+    void updatePublishableKeyHeader();
+
+    /**
+     * @brief Append function for QQmlListProperty.
+     * @param list
+     * @param customer
+     */
+    static void appendCustomer(QQmlListProperty<Customer> *list, Customer *customer);
+
+    /**
+     * @brief Count function for QQmlListProperty.
+     * @param list
+     * @return
+     */
+    static int customerCount(QQmlListProperty<Customer> *list);
+
+    /**
+     * @brief At function for QQmlListProperty.
+     * @param list
+     * @param index
+     * @return
+     */
+    static Customer *customer(QQmlListProperty<Customer> *list, int index);
+
+    /**
+     * @brief Clear function for QQmlListProperty.
+     * @param list
+     */
+    static void clearCustomers(QQmlListProperty<Customer> *list);
 };
 
 }
--- src/Stripe.cpp
+++ src/Stripe.cpp
@@ -5,8 +5,110 @@ namespace QStripe
 
 Stripe::Stripe(QObject *parent)
     : QObject(parent)
+    , m_PublishableKey("")
+    , m_SecretKey("")
+    , m_APIVersion("")
+    , m_Customers()
+    , m_NetworkUtils()
 {
 
+}
+
+QString Stripe::publishableKey() const
+{
+    return m_PublishableKey;
+}
+
+void Stripe::setPublishableKey(const QString &key)
+{
+    const bool changed = key != m_PublishableKey;
+    if (changed) {
+        m_PublishableKey = key;
+        emit publishableKeyChanged();
+    }
+}
+
+QString Stripe::secretKey() const
+{
+    return m_SecretKey;
+}
+
+void Stripe::setSecretKey(const QString &key)
+{
+    const bool changed = key != m_SecretKey;
+    if (changed) {
+        m_SecretKey = key;
+        emit secretKeyChanged();
+    }
+}
+
+QString Stripe::apiVersion() const
+{
+    return m_APIVersion;
+}
+
+void Stripe::setApiVersion(const QString &version)
+{
+    const bool changed = version != m_APIVersion;
+    if (changed) {
+        m_PublishableKey = version;
+        emit apiVersionChanged();
+    }
+}
+
+QQmlListProperty<Customer> Stripe::customers()
+{
+    return QQmlListProperty<Customer>(this, this, &Stripe::appendCustomer, &Stripe::customerCount, &Stripe::customer, &Stripe::clearCustomers);
+}
+
+void Stripe::appendCustomer(Customer *customer)
+{
+    m_Customers.append(customer);
+}
+
+int Stripe::customerCount() const
+{
+    return m_Customers.count();
+}
+
+Customer *Stripe::customer(int index) const
+{
+    return m_Customers.at(index);
+}
+
+void Stripe::clearCustomers()
+{
+    return m_Customers.clear();
+}
+
+void Stripe::updateVersionHeader()
+{
+    m_NetworkUtils.setHeader("Stripe-Version", m_APIVersion);
+}
+
+void Stripe::updatePublishableKeyHeader()
+{
+    m_NetworkUtils.setHeader("Authorization", "Bearer " + m_PublishableKey);
+}
+
+void Stripe::appendCustomer(QQmlListProperty<Customer> *list, Customer *customer)
+{
+    reinterpret_cast<Stripe *>(list->data)->appendCustomer(customer);
+}
+
+int Stripe::customerCount(QQmlListProperty<Customer> *list)
+{
+    return reinterpret_cast<Stripe *>(list->data)->customerCount();
+}
+
+Customer *Stripe::customer(QQmlListProperty<Customer> *list, int index)
+{
+    return reinterpret_cast<Stripe *>(list->data)->customer(index);
+}
+
+void Stripe::clearCustomers(QQmlListProperty<Customer> *list)
+{
+    reinterpret_cast<Stripe *>(list->data)->clearCustomers();
 }
 
 }
