851067d7f4ca779dd90616e260a384b2b65129b1
Furkan Üzümcü <furkanuzumcu@gmail.com>
2018-03-07 16:25:49 -0500

    Add card properties

--- include/QStripe/Card.h
+++ include/QStripe/Card.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <QObject>
 #include <QVector>
+// QStripe
+#include "Address.h"
 
 namespace QStripe
 {
@@ -9,36 +11,50 @@ class Card : public QObject
 {
     Q_OBJECT
 
+    Q_PROPERTY(QString cardID READ cardID NOTIFY cardIDChanged)
+    Q_PROPERTY(QString city READ city WRITE setCity NOTIFY cityChanged)
+    Q_PROPERTY(Address *address READ address WRITE setAddress NOTIFY addressChanged)
+
+    Q_PROPERTY(CardBrand brand READ brand WRITE setBrand NOTIFY brandChanged)
+    Q_PROPERTY(QString country READ country WRITE setCountry NOTIFY countryChanged)
+    Q_PROPERTY(QString currency READ currency WRITE setCurrency NOTIFY currencyChanged)
+
+    Q_PROPERTY(CVCCheck cvcCheck READ cvcCheck WRITE setCVCCheck NOTIFY cvcCheckChanged)
+    Q_PROPERTY(unsigned int expirationMonth READ expirationMonth WRITE setExpirationMonth NOTIFY expirationMonthChanged)
+    Q_PROPERTY(unsigned int expirationYear READ expirationYear WRITE setExpirationYear NOTIFY expirationYearChanged)
+
+    Q_PROPERTY(QString fingerprint READ fingerprint WRITE setFingerprint NOTIFY fingerprintChanged)
+    Q_PROPERTY(FundingType funding READ funding WRITE setFunding NOTIFY fundingChanged)
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+
+    Q_PROPERTY(QString lastFourDigits READ lastFourDigits WRITE setLastFourDigits NOTIFY lastFourDigitsChanged)
+    Q_PROPERTY(TokenizationMethod tokenizationMethod READ tokenizationMethod WRITE setTokenizationMethod NOTIFY tokenizationMethodChanged)
+    Q_PROPERTY(QVariantMap metaData READ metaData WRITE setMetaData NOTIFY metaDataChanged)
+
+    Q_PROPERTY(QString cardNumber READ cardNumber WRITE setCardNumber NOTIFY cardNumberChanged)
+
 public:
+    static const QString FIELD_ID;
     static const QString FIELD_OBJECT;
     static const QString FIELD_ADDRESS_CITY;
-    static const QString FIELD_ADDRESS_COUNTRY;
 
-    static const QString FIELD_ADDRESS_LINE1;
-    static const QString FIELD_ADDRESS_LINE1_CHECK;
-    static const QString FIELD_ADDRESS_LINE2;
-
-    static const QString FIELD_ADDRESS_STATE;
-    static const QString FIELD_ADDRESS_ZIP;
-    static const QString FIELD_ADDRESS_ZIP_CHECK;
-
+    static const QString FIELD_ADDRESS_PREFIX;
     static const QString FIELD_BRAND;
     static const QString FIELD_COUNTRY;
+
     static const QString FIELD_CURRENCY;
-
-    static const QString FIELD_CUSTOMER;
     static const QString FIELD_CVC_CHECK;
-    static const QString FIELD_EXP_MONTH;
 
+    static const QString FIELD_EXP_MONTH;
     static const QString FIELD_EXP_YEAR;
     static const QString FIELD_FINGERPRINT;
-    static const QString FIELD_FUNDING;
 
+    static const QString FIELD_FUNDING;
     static const QString FIELD_NAME;
     static const QString FIELD_LAST4;
-    static const QString FIELD_ID;
 
     static const QString FIELD_TOKENIZATION_METHOD;
+    static const QString FIELD_METADATA;
 
     enum CardBrand {
         AmericanExpress,
@@ -49,6 +65,7 @@ public:
         MasterCard,
         Unknown
     };
+    Q_ENUM(CardBrand);
 
     enum FundingType {
         FundingCredit,
@@ -56,11 +73,368 @@ public:
         FundingPrePaid,
         FundingUnknown
     };
+    Q_ENUM(FundingType);
+
+    enum CVCCheck {
+        CVCCheckPass,
+        CVCCheckFail,
+        CVCCheckUnavailable,
+        CVCCheckUnchecked,
+        CVCCheckUnknown
+    };
+    Q_ENUM(CVCCheck);
+
+    enum TokenizationMethod {
+        ApplePay,
+        GooglePay,
+        TokenizationUnknown
+    };
+    Q_ENUM(TokenizationMethod);
 
 public:
     explicit Card(QObject *parent = nullptr);
 
+    /**
+     * @brief Returns the name of the brand.
+     * @param brand
+     * @return QString
+     */
+    static QString cardBrandName(CardBrand brand);
+
+    /**
+     * @brief Returns the CardBrand enum from the name.
+     * @param name
+     * @return CardBrand
+     */
+    static CardBrand cardBrandType(const QString &name);
+
+    /**
+     * @brief Returns the string representation of the funding type.
+     * @param type
+     * @return QString
+     */
+    static QString fundingTypeString(FundingType type);
+
+    /**
+     * @brief Returns the FundingType enum for the given name.
+     * @param name
+     * @return FundingType
+     */
+    static FundingType fundingType(const QString &name);
+
+    /**
+     * @brief Returns the CVCCheck type from the name.
+     * @param name
+     * @return CVCCheck
+     */
+    static CVCCheck cvcCheckType(const QString &name);
+
+    /**
+     * @brief Returns the name of the cvc check type.
+     * @param type
+     * @return QString
+     */
+    static QString cvcCheckName(CVCCheck type);
+
+    /**
+     * @brief The default value is empty string.
+     * @return QString
+     */
+    QString cardID() const;
+
+    /**
+     * @brief Returns the city.
+     * @return QString
+     */
+    QString city() const;
+
+    /**
+     * @brief Set city name.
+     * @param ct
+     */
+    void setCity(const QString &ct);
+
+    /**
+     * @brief Returns the current address object.
+     * @return Address*
+     */
+    Address *address();
+
+    /**
+     * @brief Sett the current address. The object is copied.
+     * @param addr
+     */
+    void setAddress(const Address *addr);
+
+    /**
+     * @brief Returns the card brand.
+     * @return CardBrand
+     */
+    CardBrand brand() const;
+
+    /**
+     * @brief Set the card brand. But note that this value will change when you set the card number.
+     * @param cardBrand
+     */
+    void setBrand(const CardBrand &cardBrand);
+
+    /**
+     * @brief Returns the country.
+     * @return  QString
+     */
+    QString country() const;
+
+    /**
+     * @brief Set the country.
+     * @param country
+     */
+    void setCountry(const QString &country);
+
+    /**
+     * @brief Returns the 3 digit currency.
+     * @return QString
+     */
+    QString currency() const;
+
+    /**
+     * @brief Sets the 3 digit currency.
+     * @param currency
+     */
+    void setCurrency(const QString &currency);
+
+    /**
+     * @brief Returns the cvc check status.
+     * @return CVCCheck
+     */
+    CVCCheck cvcCheck() const;
+
+    /**
+     * @brief Set cvc check.
+     * @param check
+     */
+    void setCVCCheck(CVCCheck check);
+
+    /**
+     * @brief Returns the expiration month.
+     * @return unsigned int
+     */
+    unsigned int expirationMonth() const;
+
+    /**
+     * @brief Set the expiration month. This cannot be bigger than 12.
+     * @param month
+     */
+    void setExpirationMonth(unsigned int month);
+
+    /**
+     * @brief Returns the expiration year. This can be a 2 digit or a 4 digit number.
+     * @return unsigned int
+     */
+    unsigned int expirationYear() const;
+
+    /**
+     * @brief Set the expiration yer. This can be a 2 digit or a 4 digit number.
+     * @param year
+     */
+    void setExpirationYear(unsigned int year);
+
+    /**
+     * @brief Returns the fingerprint.
+     * @return QString
+     */
+    QString fingerprint() const;
+
+    /**
+     * @brief Set the fingerprint.
+     * @param fingerprint
+     */
+    void setFingerprint(const QString &fingerprint);
+
+    /**
+     * @brief Returns the funding type.
+     * @return FundingType
+     */
+    FundingType funding() const;
+
+    /**
+     * @brief Set funding type.
+     * @param type
+     */
+    void setFunding(const FundingType &type);
+
+    /**
+     * @brief Returns the cardholder name.
+     * @return QString
+     */
+    QString name() const;
+
+    /**
+     * @brief Set the cardholder name.
+     * @param name
+     */
+    void setName(const QString &name);
+
+    /**
+     * @brief Returns the last four digits. Last four digits can be saved in a PCI compliant way.
+     * @return QString
+     */
+    QString lastFourDigits() const;
+
+    /**
+     * @brief Set the last four digits. This value change when you set a valid card number.
+     * @param lastDigits
+     */
+    void setLastFourDigits(const QString &lastDigits);
+
+    /**
+     * @brief Returns the tokenization method.
+     * @return TokenizationMethod
+     */
+    TokenizationMethod tokenizationMethod() const;
+
+    /**
+     * @brief Set tokenization method.
+     * @param method
+     */
+    void setTokenizationMethod(const TokenizationMethod &method);
+
+    /**
+     * @brief Returns the metadata.
+     * @return QVariantMap
+     */
+    QVariantMap metaData() const;
+
+    /**
+     * @brief Set the metadata.
+     * @param data
+     */
+    void setMetaData(const QVariantMap &data);
+
+    /**
+     * @brief Returns the card number. If this card instance was created from JSON, this string will be empty since this information is not saved on Stripe.
+     * @return QString
+     */
+    QString cardNumber() const;
+
+    /**
+     * @brief Set card number. This will also cause the last four digits to be changed as well. The card brand will also be reset based on the new number.
+     * @param number
+     */
+    void setCardNumber(const QString &number);
+
+signals:
+    /**
+     * @brief Emitted when the card ID changes.
+     */
+    void cardIDChanged();
+
+    /**
+     * @brief Emitted when the city changes.
+     */
+    void cityChanged();
+
+    /**
+     * @brief Emitted when the address changes.
+     */
+    void addressChanged();
+
+    /**
+     * @brief Emitted when the brand changes.
+     */
+    void brandChanged();
+
+    /**
+     * @brief Emitted when the country changes.
+     */
+    void countryChanged();
+
+    /**
+     * @brief Emitted when the currency changes.
+     */
+    void currencyChanged();
+
+    /**
+     * @brief Emitted when the cvc check changes.
+     */
+    void cvcCheckChanged();
+
+    /**
+     * @brief Emitted when the expiration month changes.
+     */
+    void expirationMonthChanged();
+
+    /**
+     * @brief Emitted when the expiration year changes.
+     */
+    void expirationYearChanged();
+
+    /**
+     * @brief Emitted when the fingerprint changes.
+     */
+    void fingerprintChanged();
+
+    /**
+     * @brief Emitted when the funding changes.
+     */
+    void fundingChanged();
+
+    /**
+     * @brief Emitted when the cardholder name changes.
+     */
+    void nameChanged();
+
+    /**
+     * @brief Emitted when the last four digits changes. This will be also emitted when a valid card number is set.
+     */
+    void lastFourDigitsChanged();
+
+    /**
+     * @brief Emitted when the tokenization method changes.
+     */
+    void tokenizationMethodChanged();
+
+    /**
+     * @brief Emitted when the metadata changes.
+     */
+    void metaDataChanged();
+
+    /**
+     * @brief Emitted when the card number changes.
+     */
+    void cardNumberChanged();
+
 private:
+    QString m_CardID;
+    QString m_City;
+    Address m_Address;
+
+    CardBrand m_Brand;
+    // This is the two letter country code that this card belongs to.
+    QString m_Country;
+    QString m_Currency;
+
+    CVCCheck m_CVCCheck;
+    unsigned int m_ExpirationMonth;
+    // This can be a 2 digit or 4 digit number.
+    unsigned int m_ExpirationYear;
+
+    QString m_Fingerprint;
+    FundingType m_FundingType;
+    QString m_Name;
+
+    QString m_LastFourDigits;
+    TokenizationMethod m_TokenizationMethod;
+    QVariantMap m_MetaData;
+
+    QString m_CardNumber;
+
+private:
+    /**
+     * @brief Card ID can only be changed internally.
+     * @param id
+     */
+    void setCardID(const QString &id);
+
     /**
      * @brief Returns the card number prefixes for the brand.
      * Card number prefixes are based on http://en.wikipedia.org/wiki/Bank_card_number#Issuer_identification_number_.28IIN.29
@@ -74,34 +448,6 @@ private:
      * @return int
      */
     int maxCardNumberLenght(CardBrand brand) const;
-
-    /**
-     * @brief Returns the name of the brand.
-     * @param brand
-     * @return QString
-     */
-    QString cardBrandName(CardBrand brand) const;
-
-    /**
-     * @brief Returns the CardBrand enum from the name.
-     * @param name
-     * @return CardBrand
-     */
-    CardBrand cardBrandType(const QString &name);
-
-    /**
-     * @brief Returns the string representation of the funding type.
-     * @param type
-     * @return QString
-     */
-    QString fundingTypeString(FundingType type) const;
-
-    /**
-     * @brief Returns the FundingType enum for the given name.
-     * @param name
-     * @return FundingType
-     */
-    FundingType fundingType(const QString &name) const;
 };
 
 }
--- src/Card.cpp
+++ src/Card.cpp
@@ -3,96 +3,50 @@
 namespace QStripe
 {
 
+const QString Card::FIELD_ID = "id";
 const QString Card::FIELD_OBJECT = "object";
 const QString Card::FIELD_ADDRESS_CITY = "address_city";
-const QString Card::FIELD_ADDRESS_COUNTRY = "address_country";
 
-const QString Card::FIELD_ADDRESS_LINE1 = "address_line1";
-const QString Card::FIELD_ADDRESS_LINE1_CHECK = "address_line1_check";
-const QString Card::FIELD_ADDRESS_LINE2 = "address_line2";
-
-const QString Card::FIELD_ADDRESS_STATE = "address_state";
-const QString Card::FIELD_ADDRESS_ZIP = "address_zip";
-const QString Card::FIELD_ADDRESS_ZIP_CHECK = "address_zip_check";
-
+const QString Card::FIELD_ADDRESS_PREFIX = "address_";
 const QString Card::FIELD_BRAND = "brand";
 const QString Card::FIELD_COUNTRY = "country";
+
 const QString Card::FIELD_CURRENCY = "currency";
-
-const QString Card::FIELD_CUSTOMER = "customer";
 const QString Card::FIELD_CVC_CHECK = "cvc_check";
-const QString Card::FIELD_EXP_MONTH = "exp_month";
 
+const QString Card::FIELD_EXP_MONTH = "exp_month";
 const QString Card::FIELD_EXP_YEAR = "exp_year";
 const QString Card::FIELD_FINGERPRINT = "fingerprint";
-const QString Card::FIELD_FUNDING = "funding";
 
+const QString Card::FIELD_FUNDING = "funding";
 const QString Card::FIELD_NAME = "name";
 const QString Card::FIELD_LAST4 = "last4";
-const QString Card::FIELD_ID = "id";
 
 const QString Card::FIELD_TOKENIZATION_METHOD = "tokenization_method";
+const QString Card::FIELD_METADATA = "metadata";
 
 Card::Card(QObject *parent)
     : QObject(parent)
+    , m_CardID("")
+    , m_City("")
+    , m_Address()
+    , m_Brand(CardBrand::Unknown)
+    , m_Country("")
+    , m_Currency("")
+    , m_CVCCheck(CVCCheck::CVCCheckUnknown)
+    , m_ExpirationMonth(0)
+    , m_ExpirationYear(0)
+    , m_Fingerprint("")
+    , m_FundingType(FundingType::FundingUnknown)
+    , m_Name("")
+    , m_LastFourDigits("")
+    , m_TokenizationMethod(TokenizationMethod::TokenizationUnknown)
+    , m_MetaData()
 {
 
 }
 
-QVector<QString> Card::prefixes(CardBrand brand) const
-{
-    QVector<QString> numberPrefixes;
-
-    if (brand == CardBrand::AmericanExpress) {
-        numberPrefixes = QVector<QString> {"34", "37"};
-    }
-    else if (brand == CardBrand::Discover) {
-        numberPrefixes = QVector<QString> {"60", "62", "64", "65"};
-    }
-    else if (brand == CardBrand::JCB) {
-        numberPrefixes = QVector<QString> {"35"};
-    }
-    else if (brand == CardBrand::DinersClub) {
-        numberPrefixes = QVector<QString> {
-            "300", "301", "302", "303", "304",
-            "305", "309", "36", "38", "39"
-        };
-    }
-    else if (brand == CardBrand::Visa) {
-        numberPrefixes = QVector<QString> {"4"};
-    }
-    else if (brand == CardBrand::MasterCard) {
-        numberPrefixes = QVector<QString> {
-            "2221", "2222", "2223", "2224",
-            "2225", "2226", "2227", "2228",
-            "2229", "223", "224", "225",
-            "226", "227", "228", "229",
-            "23", "24", "25", "26",
-            "270", "271", "2720", "50",
-            "51", "52", "53", "54",
-            "55", "67"
-        };
-    }
-
-    return numberPrefixes;
-}
-
-int Card::maxCardNumberLenght(CardBrand brand) const
-{
-    // This is the standard one.
-    int length = 16;
-
-    if (brand == CardBrand::AmericanExpress) {
-        length = 15;
-    }
-    else if (brand == CardBrand::DinersClub) {
-        length = 14;
-    }
-
-    return length;
-}
-
-QString Card::cardBrandName(CardBrand brand) const
+QString Card::cardBrandName(CardBrand brand)
 {
     QString name;
     if (brand == CardBrand::AmericanExpress) {
@@ -148,7 +102,7 @@ Card::CardBrand Card::cardBrandType(const QString &name)
     return brand;
 }
 
-QString Card::fundingTypeString(FundingType type) const
+QString Card::fundingTypeString(FundingType type)
 {
     QString name;
     if (type == FundingType::FundingCredit) {
@@ -167,7 +121,7 @@ QString Card::fundingTypeString(FundingType type) const
     return name;
 }
 
-Card::FundingType Card::fundingType(const QString &name) const
+Card::FundingType Card::fundingType(const QString &name)
 {
     FundingType type;
     if (name == "credit") {
@@ -184,6 +138,265 @@ Card::FundingType Card::fundingType(const QString &name) const
     }
 
     return type;
+}
+
+Card::CVCCheck Card::cvcCheckType(const QString &name)
+{
+    CVCCheck type = CVCCheck::CVCCheckUnknown;
+    if (name == "pass") {
+        type = CVCCheck::CVCCheckPass;
+    }
+    else if (name == "fail") {
+        type = CVCCheck::CVCCheckFail;
+    }
+    else if (name == "unavailable") {
+        type = CVCCheck::CVCCheckUnavailable;
+    }
+    else if (name == "unchecked") {
+        type = CVCCheck::CVCCheckUnchecked;
+    }
+
+    return type;
+}
+
+QString Card::cvcCheckName(CVCCheck type)
+{
+    QString name = "unknown";
+    if (type == CVCCheck::CVCCheckPass) {
+        name = "pass";
+    }
+    else if (type == CVCCheck::CVCCheckFail) {
+        name = "fail";
+    }
+    else if (type == CVCCheck::CVCCheckUnavailable) {
+        name = "unavailable";
+    }
+    else if (type == CVCCheck::CVCCheckUnchecked) {
+        name = "unchecked";
+    }
+
+    return name;
+}
+
+QString Card::cardID() const
+{
+    return m_CardID;
+}
+
+QString Card::city() const
+{
+    return m_City;
+}
+
+void Card::setCity(const QString &ct)
+{
+    m_City = ct;
+}
+
+Address *Card::address()
+{
+    return &m_Address;
+}
+
+void Card::setAddress(const Address *addr)
+{
+    const bool changed = m_Address != (*addr);
+    if (changed) {
+        m_Address.set(*addr);
+        emit addressChanged();
+    }
+}
+
+Card::CardBrand Card::brand() const
+{
+    return m_Brand;
+}
+
+void Card::setBrand(const CardBrand &cardBrand)
+{
+    m_Brand = cardBrand;
+}
+
+QString Card::country() const
+{
+    return m_Country;
+}
+
+void Card::setCountry(const QString &country)
+{
+    m_Country = country;
+}
+
+QString Card::currency() const
+{
+    return m_Currency;
+}
+
+void Card::setCurrency(const QString &currency)
+{
+    m_Currency = currency;
+}
+
+Card::CVCCheck Card::cvcCheck() const
+{
+    return m_CVCCheck;
+}
+
+void Card::setCVCCheck(CVCCheck check)
+{
+    m_CVCCheck = check;
+}
+
+unsigned int Card::expirationMonth() const
+{
+    return m_ExpirationMonth;
+}
+
+void Card::setExpirationMonth(unsigned int month)
+{
+    m_ExpirationMonth = month;
+}
+
+unsigned int Card::expirationYear() const
+{
+    return m_ExpirationYear;
+}
+
+void Card::setExpirationYear(unsigned int year)
+{
+    m_ExpirationYear = year;
+}
+
+QString Card::fingerprint() const
+{
+    return m_Fingerprint;
+}
+
+void Card::setFingerprint(const QString &fingerprint)
+{
+    m_Fingerprint = fingerprint;
+}
+
+Card::FundingType Card::funding() const
+{
+    return m_FundingType;
+}
+
+void Card::setFunding(const FundingType &type)
+{
+    m_FundingType = type;
+}
+
+QString Card::name() const
+{
+    return m_Name;
+}
+
+void Card::setName(const QString &name)
+{
+    m_Name = name;
+}
+
+QString Card::lastFourDigits() const
+{
+    return m_LastFourDigits;
+}
+
+void Card::setLastFourDigits(const QString &lastDigits)
+{
+    m_LastFourDigits = lastDigits;
+}
+
+Card::TokenizationMethod Card::tokenizationMethod() const
+{
+    return m_TokenizationMethod;
+}
+
+void Card::setTokenizationMethod(const TokenizationMethod &method)
+{
+    m_TokenizationMethod = method;
+}
+
+QVariantMap Card::metaData() const
+{
+    return m_MetaData;
+}
+
+void Card::setMetaData(const QVariantMap &data)
+{
+    m_MetaData = data;
+}
+
+QString Card::cardNumber() const
+{
+    return m_CardNumber;
+}
+
+void Card::setCardNumber(const QString &number)
+{
+    m_CardNumber = number;
+}
+
+void Card::setCardID(const QString &id)
+{
+    const bool changed = m_CardID != id;
+    if (changed) {
+        m_CardID = id;
+        emit cardIDChanged();
+    }
+}
+
+QVector<QString> Card::prefixes(CardBrand brand) const
+{
+    QVector<QString> numberPrefixes;
+
+    if (brand == CardBrand::AmericanExpress) {
+        numberPrefixes = QVector<QString> {"34", "37"};
+    }
+    else if (brand == CardBrand::Discover) {
+        numberPrefixes = QVector<QString> {"60", "62", "64", "65"};
+    }
+    else if (brand == CardBrand::JCB) {
+        numberPrefixes = QVector<QString> {"35"};
+    }
+    else if (brand == CardBrand::DinersClub) {
+        numberPrefixes = QVector<QString> {
+            "300", "301", "302", "303", "304",
+            "305", "309", "36", "38", "39"
+        };
+    }
+    else if (brand == CardBrand::Visa) {
+        numberPrefixes = QVector<QString> {"4"};
+    }
+    else if (brand == CardBrand::MasterCard) {
+        numberPrefixes = QVector<QString> {
+            "2221", "2222", "2223", "2224",
+            "2225", "2226", "2227", "2228",
+            "2229", "223", "224", "225",
+            "226", "227", "228", "229",
+            "23", "24", "25", "26",
+            "270", "271", "2720", "50",
+            "51", "52", "53", "54",
+            "55", "67"
+        };
+    }
+
+    return numberPrefixes;
+}
+
+int Card::maxCardNumberLenght(CardBrand brand) const
+{
+    // This is the standard one.
+    int length = 16;
+
+    if (brand == CardBrand::AmericanExpress) {
+        length = 15;
+    }
+    else if (brand == CardBrand::DinersClub) {
+        length = 14;
+    }
+
+    return length;
 }
 
 }
