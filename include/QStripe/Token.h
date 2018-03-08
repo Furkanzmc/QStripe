e82c2b1a4a6920585fc3c3e12c044cd914b2c9c3
Furkan Üzümcü <furkanuzumcu@gmail.com>
2018-03-08 09:04:28 -0500

    Add properties.

--- include/QStripe/Token.h
+++ include/QStripe/Token.h
@@ -1,19 +1,111 @@
 #pragma once
 #include <QObject>
+#include <QDateTime>
+// QStripe
+#include "Card.h"
 
 namespace QStripe
 {
 
+/**
+ * @brief Token is a read-only class and it can only be created using `QStripe::Stripe::createToken()`.
+ * The properties does not have a related changed signals.
+ */
 class Token : public QObject
 {
     Q_OBJECT
 
+    Q_PROPERTY(QVariantMap bankAccount READ bankAccount CONSTANT)
+    Q_PROPERTY(const Card *card READ card CONSTANT)
+    Q_PROPERTY(QDateTime created READ created CONSTANT)
+
+    Q_PROPERTY(QString tokenID READ tokenID CONSTANT)
+    Q_PROPERTY(bool liveMode READ liveMode CONSTANT)
+    Q_PROPERTY(bool used READ used CONSTANT)
+
+public:
+    static const QString FIELD_BANK_ACCOUNT;
+    static const QString FIELD_CARD;
+    static const QString FIELD_CREATED;
+
+    static const QString FIELD_ID;
+    static const QString FIELD_LIVEMODE;
+    static const QString FIELD_TYPE;
+
+    static const QString FIELD_USED;
+
+    enum Type {
+        TypeBankAccount,
+        TypeCard,
+        TypePII,
+        TypeAccount,
+        TypeUnknown // This does not exist in the API.
+    };
+    Q_ENUM(Type);
+
 public:
     explicit Token(QObject *parent = nullptr);
 
-signals:
+    /**
+     * @brief Returns the card that belongs to this token.
+     * @return const Card *
+     */
+    const Card *card() const;
 
-public slots:
+    /**
+     * @brief Returns the bank account. In the future release this will be a class of its own.
+     * @return QVariantMap
+     */
+    QVariantMap bankAccount() const;
+
+    /**
+     * @brief Returns the created time in UTC format.
+     * @note When Qt converts the QDateTime to Date object, the timezone information is lost. (See https://bugreports.qt.io/browse/QTBUG-25262)
+     * @return
+     */
+    QDateTime created() const;
+
+    /**
+     * @brief Returns the token ID.
+     * @return QString
+     */
+    QString tokenID() const;
+
+    /**
+     * @brief Flag indicating whether the object exists in live mode or test mode.
+     * @return bool
+     */
+    bool liveMode() const;
+
+    /**
+     * @brief Whether this token has already been used (tokens can be used only once).
+     * @return bool
+     */
+    bool used() const;
+
+    /**
+     * @brief Returns the string representation of the given Type.
+     * @param type
+     * @return QString
+     */
+    static QString typeName(Type type);
+
+    /**
+     * @brief Returns the type from its string representation.
+     * @param name
+     * @return Type
+     */
+    static Type typeEnum(const QString &name);
+
+private:
+    // FIXME: Create a seperate class for BankAccount.
+    QVariantMap m_BankAccount;
+    Card m_Card;
+    QDateTime m_Created;
+
+    QString m_TokenID;
+    bool m_IsLiveMode;
+    bool m_IsUsed;
 };
 
 }
--- src/Token.cpp
+++ src/Token.cpp
@@ -3,10 +3,96 @@
 namespace QStripe
 {
 
+const QString Token::FIELD_BANK_ACCOUNT = "bank_account";
+const QString Token::FIELD_CARD = "card";
+const QString Token::FIELD_CREATED = "created";
+
+const QString Token::FIELD_ID = "id";
+const QString Token::FIELD_LIVEMODE = "livemode";
+const QString Token::FIELD_TYPE = "type";
+
+const QString Token::FIELD_USED = "used";
+
 Token::Token(QObject *parent)
     : QObject(parent)
+    , m_BankAccount()
+    , m_Card()
+    , m_Created()
+    , m_TokenID("")
+    , m_IsLiveMode(false)
+    , m_IsUsed(false)
 {
 
+}
+
+const Card *Token::card() const
+{
+    return &m_Card;
+}
+
+QVariantMap Token::bankAccount() const
+{
+    return m_BankAccount;
+}
+
+QDateTime Token::created() const
+{
+    return m_Created;
+}
+
+QString Token::tokenID() const
+{
+    return m_TokenID;
+}
+
+bool Token::liveMode() const
+{
+    return m_IsLiveMode;
+}
+
+bool Token::used() const
+{
+    return m_IsUsed;
+}
+
+QString Token::typeName(Type type)
+{
+    QString name = "unknown";
+
+    if (type == Type::TypeBankAccount) {
+        name = "bank_account";
+    }
+    else if (type == Type::TypeCard) {
+        name = "card";
+    }
+    else if (type == Type::TypePII) {
+        name = "pii";
+    }
+    else if (type == Type::TypeAccount) {
+        name = "account";
+    }
+
+    return name;
+}
+
+Token::Type Token::typeEnum(const QString &name)
+{
+    Type type = Type::TypeUnknown;
+
+    if (name == "bank_account") {
+        type = Type::TypeBankAccount;
+    }
+    else if (name == "card") {
+        type = Type::TypeCard;
+    }
+    else if (name == "pii") {
+        type = Type::TypePII;
+    }
+    else if (name == "account") {
+        type = Type::TypeAccount;
+    }
+
+    return type;
 }
 
 }
